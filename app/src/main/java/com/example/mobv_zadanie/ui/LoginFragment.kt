f5ced63a0a84e9749a42fde7579d37ce20e21743
MartinVoronak <voronak.martin@gmail.com>
2019-11-23 21:15:17 +0100

    login gui

--- app/src/main/java/com/example/mobv_zadanie/ui/LoginFragment.kt
+++ app/src/main/java/com/example/mobv_zadanie/ui/LoginFragment.kt
@@ -2,12 +2,16 @@ package com.example.mobv_zadanie.ui
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 import com.example.mobv_zadanie.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +26,15 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val login = view?.findViewById<EditText>(R.id.edit_login_name)
+        val passwd = view?.findViewById<EditText>(R.id.edit_login_passw)
+
+        btn_login.setOnClickListener { view ->
+            Log.i("TAG_LOGIN", "Login pressed, name:"+login.text.toString()+"   password:"+passwd.text.toString())
+        }
+    }
 
 }
